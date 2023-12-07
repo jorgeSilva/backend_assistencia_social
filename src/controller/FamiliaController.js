f7faacf1981c7a56c1442f52d448484a97385bdd
jorgeSilva <jorgelfsilva5@gmail.com>
2023-12-07 15:58:14 -0300

    [feat] - adição da rota de exclusão de registro da tabela familia

--- src/controller/FamiliaController.js
+++ src/controller/FamiliaController.js
@@ -175,6 +175,18 @@ class FamiliaController{
     ).then(r => res.status(200).json(r)).catch((e) => res.status(400).json(e))
 
   }
+
+  async delete(req, res){
+    const {_id } = req.params
+
+    if(!_id){
+      return res.status(400).json({error: `Id inválido: ${_id}`})
+    }
+
+    await Familia.deleteOne({'_id': _id})
+      .then(() => res.status(200).json({msg:'Deletado com sucesso.'}))
+        .catch(() => res.status(400).json({error: 'Não foi encontrado o produto informado'}))
+  }
 }
 
 module.exports = new FamiliaController()
--- src/routes/FamiliaRoute.js
+++ src/routes/FamiliaRoute.js
@@ -5,10 +5,10 @@ const FamiliaController = require('../controller/FamiliaController')
 const UserMiddleware = require('../middleware/UserMiddleware')
 
 familiaRouter.post('/familia', FamiliaController.store)
-
 familiaRouter.put('/familia/update/:_id', FamiliaController.update)
+familiaRouter.delete('/familia/delete/:_id', UserMiddleware, FamiliaController.delete)
 
-familiaRouter.get('/familia/show',UserMiddleware, FamiliaController.show)
-familiaRouter.get('/familia/:_id',UserMiddleware, FamiliaController.index)
+familiaRouter.get('/familia/show', UserMiddleware, FamiliaController.show)
+familiaRouter.get('/familia/:_id', UserMiddleware, FamiliaController.index)
 
 module.exports = familiaRouter
