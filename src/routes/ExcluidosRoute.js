5004c035d6f50efd52dd190565b79d6d84dbabf5
jorgeSilva <jorgelfsilva5@gmail.com>
2023-12-07 14:44:21 -0300

    [refactor] - Adição do middleware nas rotas get do site

--- src/routes/ExcluidosRoute.js
+++ src/routes/ExcluidosRoute.js
@@ -2,10 +2,11 @@ const express = require('express')
 const ExcluidosRoutes = express.Router()
 
 const ExcluidosController = require('../controller/ExcluidosController')
+const UserMiddleware = require('../middleware/UserMiddleware')
 
 ExcluidosRoutes.post('/familia/excluidos', ExcluidosController.store)
 
-ExcluidosRoutes.get('/familia/excluidos/show', ExcluidosController.show)
-ExcluidosRoutes.get('/familia/excluidos/:_id', ExcluidosController.index)
+ExcluidosRoutes.get('/familia/excluidos/show',UserMiddleware, ExcluidosController.show)
+ExcluidosRoutes.get('/familia/excluidos/:_id',UserMiddleware, ExcluidosController.index)
 
 module.exports = ExcluidosRoutes
--- src/routes/FamiliaRoute.js
+++ src/routes/FamiliaRoute.js
@@ -2,10 +2,11 @@ const express = require('express')
 const familiaRouter = express.Router()
 
 const FamiliaController = require('../controller/FamiliaController')
+const UserMiddleware = require('../middleware/UserMiddleware')
 
 familiaRouter.post('/familia', FamiliaController.store)
 
-familiaRouter.get('/familia/show', FamiliaController.show)
-familiaRouter.get('/familia/:_id', FamiliaController.index)
+familiaRouter.get('/familia/show',UserMiddleware, FamiliaController.show)
+familiaRouter.get('/familia/:_id',UserMiddleware, FamiliaController.index)
 
 module.exports = familiaRouter
