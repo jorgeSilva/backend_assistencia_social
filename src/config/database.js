df32f6755921b25d016dd2e107d2bc6190875ff7
jorgeSilva <jorgelfsilva5@gmail.com>
2024-01-16 11:00:52 -0300

    fix - remoção da variavel de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,7 @@ require('dotenv').config()
 
 const dbUser = process.env.DB_USER
 const dbPass = process.env.DB_PASS
-const dbName = process.env.DB_NAME
 
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@dbassitenciasocial.ckejrr6.mongodb.net/${dbName}?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@dbassitenciasocial.ckejrr6.mongodb.net/?retryWrites=true&w=majority`)
 
 module.exports = mongoose
